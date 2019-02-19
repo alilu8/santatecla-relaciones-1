1dd83644fe5b34ef834cadb6ce3be79d0c98df0c
CarlosFco <carlosfc1996@hotmail.com>
2019-02-19 17:32:24 +0100

    updated UnitController.java

--- backend/relaciones/src/main/java/es/santatecla/controllers/UnitController.java
+++ backend/relaciones/src/main/java/es/santatecla/controllers/UnitController.java
@@ -1,9 +1,9 @@
 package es.santatecla.controllers;
 
-import javax.enterprise.inject.Model;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.*;
 
 @Controller
 public class UnitController {
@@ -15,9 +15,13 @@ public class UnitController {
 		for (int i = 0; i < unitsArray.length; i++) {
 			unitsArray[i] = i;
 		}
-		model.addAttribute("units", unitsArray );
+		model.addAttribute("units", "holi");
 		
 		return "index";
+		
+	}
+	
+	public static void main(String[] args) {
 		
 	}
 	
